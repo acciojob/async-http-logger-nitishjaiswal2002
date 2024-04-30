95ef671d9f1279648e790c5fbe2e0d7db137e550
Nitish Jaiswal <141999440+nitishjaiswal2002@users.noreply.github.com>
2024-04-30 14:25:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+async function fetchData() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+fetchData();
